bc8e810c137a2051b2bfe1f18376bdc4754784cc
jon roig <jroig@godaddy.com>
2019-08-05 13:51:38 -0700

    cleanup

--- mailtowa.js
+++ mailtowa.js
@@ -4,8 +4,6 @@
 // https://outlook.office.com/owa/?path=/mail/action/compose&to={to}&subject={subject}&body={body}&cc={cc}&bcc={bcc}"
 
 const handleLink = (e) => {
-    console.log('handleLink', e);
-
     theTarget = e.target;
     if (theTarget.tagName.toLowerCase() == 'a' && theTarget.href.substr(0,7) === 'mailto:') {
         chrome.storage.sync.get('owaIntercept', (data) => {
@@ -14,11 +12,12 @@ const handleLink = (e) => {
                 const paramString = `to=${mailtoLink}`;
                 const outlookString = `https://outlook.office.com/owa/?path=/mail/action/compose&${paramString}`;
                 window.open(outlookString, '_blank');
+
+                e.preventDefault();
+                return false;
             }
         });
     }
-
-
 }
 
 document.addEventListener("click", handleLink, false);
