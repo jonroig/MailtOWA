f7de177eda870b26d4485225723b20eb7f41b08c
Felix Dombek <felix_dombek@hotmail.com>
2020-11-28 17:06:53 +0100

    Add Outlook.Live

--- mailtowa.js
+++ mailtowa.js
@@ -1,16 +1,17 @@
 // main link intercept...
 
 // an example OWA compose URL:
-// https://outlook.office.com/owa/?path=/mail/action/compose&to={to}&subject={subject}&body={body}&cc={cc}&bcc={bcc}"
+// https://outlook.{office|live}.com/owa/?path=/mail/action/compose&to={to}&subject={subject}&body={body}&cc={cc}&bcc={bcc}"
 
 const handleLink = (e) => {
     theTarget = e.target;
     if (theTarget.tagName.toLowerCase() == 'a' && theTarget.href.substr(0,7) === 'mailto:') {
         chrome.storage.sync.get('owaIntercept', (data) => {
-            if (data.owaIntercept) {
+            if (String(data.owaIntercept).startsWith("on")) {
                 const mailtoLink = theTarget.href.substr(7).replace('?', '&');
                 const paramString = `to=${mailtoLink}`;
-                const outlookString = `https://outlook.office.com/owa/?path=/mail/action/compose&${paramString}`;
+                const urlString = data.owaIntercept.length === 2 ? "office" : "live";
+                const outlookString = `https://outlook.${urlString}.com/owa/?path=/mail/action/compose&${paramString}`;
                 window.open(outlookString, '_blank');
 
                 e.preventDefault();
--- popup.js
+++ popup.js
@@ -1,41 +1,70 @@
 'use strict';
 
 const interceptOn = document.getElementById('interceptOn');
+const interceptOnLive = document.getElementById('interceptOnLive');
 const interceptOff = document.getElementById('interceptOff');
 const interceptOnText = document.getElementById('interceptOnText');
+const interceptOnLiveText = document.getElementById('interceptOnLiveText');
 const interceptOffText = document.getElementById('interceptOffText');
 
 chrome.storage.sync.get('owaIntercept', (data) => {
     console.log('data', data);
-    if (data.owaIntercept) {
+    if (data.owaIntercept === "on") {
         interceptOn.checked = true;
+        interceptOnLive.checked = false;
         interceptOff.checked = false;
         interceptOnText.style.fontWeight = 'bold';
+        interceptOnLive.style.fontWeight = 'normal';
+        interceptOffText.style.fontWeight = 'normal';
+    } else if (data.owaIntercept === "on_live") {
+        interceptOn.checked = false;
+        interceptOnLive.checked = true;
+        interceptOff.checked = false;
+        interceptOnText.style.fontWeight = 'normal';
+        interceptOnLiveText.style.fontWeight = 'bold';
         interceptOffText.style.fontWeight = 'normal';
     } else {
         interceptOn.checked = false;
+        interceptOnLive.checked = false;
         interceptOff.checked = true;
         interceptOnText.style.fontWeight = 'normal';
+        interceptOnLiveText.style.fontWeight = 'normal';
         interceptOffText.style.fontWeight = 'bold';
     }
 });
 
 interceptOn.onclick = () => {
-    chrome.storage.sync.set({owaIntercept: true}, () => {
+    chrome.storage.sync.set({owaIntercept: "on"}, () => {
       console.log('intercept on');
       interceptOn.checked = true;
+      interceptOnLive.checked = false;
       interceptOff.checked = false;
       interceptOnText.style.fontWeight = 'bold';
+      interceptOnLive.style.fontWeight = 'normal';
+      interceptOffText.style.fontWeight = 'normal';
+  });
+}
+
+interceptOnLive.onclick = () => {
+    chrome.storage.sync.set({owaIntercept: "on_live"}, () => {
+      console.log('intercept on_live');
+      interceptOn.checked = false;
+      interceptOnLive.checked = true;
+      interceptOff.checked = false;
+      interceptOnText.style.fontWeight = 'normal';
+      interceptOnLiveText.style.fontWeight = 'bold';
       interceptOffText.style.fontWeight = 'normal';
   });
 }
 
 interceptOff.onclick = () => {
-    chrome.storage.sync.set({owaIntercept: false}, () => {
+    chrome.storage.sync.set({owaIntercept: "off"}, () => {
       console.log('intercept off');
       interceptOn.checked = false;
+      interceptOnLive.checked = false;
       interceptOff.checked = true;
       interceptOnText.style.fontWeight = 'normal';
+      interceptOnLiveText.style.fontWeight = 'normal';
       interceptOffText.style.fontWeight = 'bold';
   });
 }
