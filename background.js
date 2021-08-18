db4bc2816bff16a54f543b59ba541195d8b81b4d
Owen Kieffer-Jones <owen.kieffer-jones@holy-ag.de>
2021-08-18 13:08:58 +0200

    Add custom domains, popup option
    +Refactoring

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({owaIntercept: "on"}, function() {
-    console.log('Intercept activated!');
-  });
+  chrome.storage.sync.get('owaIntercept', (data) => {
+    if (!data.owaIntercept) {
+      chrome.storage.sync.set({owaIntercept: "on"}, function() {
+        console.log('Intercept activated!');
+      });
+    }
+  })
 });
--- popup.js
+++ popup.js
@@ -1,70 +1,24 @@
 'use strict';
 
-const interceptOn = document.getElementById('interceptOn');
-const interceptOnLive = document.getElementById('interceptOnLive');
-const interceptOff = document.getElementById('interceptOff');
-const interceptOnText = document.getElementById('interceptOnText');
-const interceptOnLiveText = document.getElementById('interceptOnLiveText');
-const interceptOffText = document.getElementById('interceptOffText');
+const radios = Array.from(document.querySelectorAll('input[name=intercept]'));
+const domainInput = document.querySelector('input[name=domain]');
+const popupCheckbox = document.querySelector('input[name=popup]');
 
-chrome.storage.sync.get('owaIntercept', (data) => {
-    console.log('data', data);
-    if (data.owaIntercept === "on") {
-        interceptOn.checked = true;
-        interceptOnLive.checked = false;
-        interceptOff.checked = false;
-        interceptOnText.style.fontWeight = 'bold';
-        interceptOnLive.style.fontWeight = 'normal';
-        interceptOffText.style.fontWeight = 'normal';
-    } else if (data.owaIntercept === "on_live") {
-        interceptOn.checked = false;
-        interceptOnLive.checked = true;
-        interceptOff.checked = false;
-        interceptOnText.style.fontWeight = 'normal';
-        interceptOnLiveText.style.fontWeight = 'bold';
-        interceptOffText.style.fontWeight = 'normal';
-    } else {
-        interceptOn.checked = false;
-        interceptOnLive.checked = false;
-        interceptOff.checked = true;
-        interceptOnText.style.fontWeight = 'normal';
-        interceptOnLiveText.style.fontWeight = 'normal';
-        interceptOffText.style.fontWeight = 'bold';
-    }
+chrome.storage.sync.get(['owaIntercept', 'owaDomain', 'owaPopup'], (data) => {
+    // Set active radio button
+    radios.find(el => el.value === data.owaIntercept).checked = true;
+    // Set custom domain
+    domainInput.value = data.owaDomain || '';
+    popupCheckbox.checked = data.owaPopup || false;
 });
 
-interceptOn.onclick = () => {
-    chrome.storage.sync.set({owaIntercept: "on"}, () => {
-      console.log('intercept on');
-      interceptOn.checked = true;
-      interceptOnLive.checked = false;
-      interceptOff.checked = false;
-      interceptOnText.style.fontWeight = 'bold';
-      interceptOnLive.style.fontWeight = 'normal';
-      interceptOffText.style.fontWeight = 'normal';
-  });
-}
-
-interceptOnLive.onclick = () => {
-    chrome.storage.sync.set({owaIntercept: "on_live"}, () => {
-      console.log('intercept on_live');
-      interceptOn.checked = false;
-      interceptOnLive.checked = true;
-      interceptOff.checked = false;
-      interceptOnText.style.fontWeight = 'normal';
-      interceptOnLiveText.style.fontWeight = 'bold';
-      interceptOffText.style.fontWeight = 'normal';
-  });
-}
-
-interceptOff.onclick = () => {
-    chrome.storage.sync.set({owaIntercept: "off"}, () => {
-      console.log('intercept off');
-      interceptOn.checked = false;
-      interceptOnLive.checked = false;
-      interceptOff.checked = true;
-      interceptOnText.style.fontWeight = 'normal';
-      interceptOnLiveText.style.fontWeight = 'normal';
-      interceptOffText.style.fontWeight = 'bold';
-  });
-}
+document.addEventListener('change',  e => {
+    // Get active radio button
+    const input = radios.find(el => el.checked);
+    // Store settings
+    chrome.storage.sync.set({
+        owaIntercept: input.value,
+        owaDomain: domainInput.value || null,
+        owaPopup: popupCheckbox.checked,
+    });
+});
